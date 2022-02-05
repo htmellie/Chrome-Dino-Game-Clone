6123f27f960e59091204bdb1feeaa59c0afaf9d1
htmellie <ellieredmayne@hotmail.com>
2022-02-05 16:43:42 +0000

    refactor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
-function jump() {
+function handleJump() {
   if (dino.classList != "jump") {
     dino.classList.add("jump");
 
@@ -11,22 +11,24 @@ function jump() {
   }
 }
 
-let isAlive = setInterval(function () {
+const isAlive = setInterval(function () {
   // get current dino Y position
-  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  const dinoPos = parseInt(
+    window.getComputedStyle(dino).getPropertyValue("top")
+  );
 
   // get current cactus X position
-  let cactusLeft = parseInt(
+  const cactusPos = parseInt(
     window.getComputedStyle(cactus).getPropertyValue("left")
   );
 
   // detect collision
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+  if (cactusPos < 50 && cactusPos > 0 && dinoPos >= 140) {
     // collision
     alert("Game Over!");
   }
 }, 10);
 
 document.addEventListener("keydown", function (event) {
-  jump();
+  handleJump();
 });
